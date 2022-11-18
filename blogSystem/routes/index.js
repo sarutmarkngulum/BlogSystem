cfbb3ac325a96339081e027c347284fa3b5afee0
sarut markngulaum <78048767+sarutmarkngulum@users.noreply.github.com>
2022-11-18 21:44:57 +0700

    Validation ข้อมูล

--- blogSystem/routes/index.js
+++ blogSystem/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+var {check,validationResult} = require('express-validator')
 //connect
 var mongodb = require('mongodb');
 var db = require('monk')('127.0.0.1:27017/BlogDB')
@@ -14,5 +15,21 @@ router.get('/', function(req, res, next) {
   })
   
 });
+
+router.get('/category/add', function(req, res, next) {
+   res.render('addcategory');
+});
+
+router.post('/category/add', [
+  check('name', 'ชื่อประเภทต้องไม่เป็นค่าว่าง').not().isEmpty()
+], function(req, res, next) {
+  var result = validationResult(req);
+  var errors = result.errors;
+  if (!result.isEmpty()) {
+    res.render('addcategory', {errors: errors});
+  }
+  res.render('addcategory') 
+});
+
 
 module.exports = router;
